d053ee199bf1d862e8ca75eeb27a8066f1cc67d1
David Ko <david.ko@damncarousel.com>
2014-07-24 14:19:31 -0400

    Quadratic Equation solver

---
+++ utilities/quadratic_equation.rb
@@ -0,0 +1,31 @@
+class QuadraticEquation
+  attr_accessor :first_root, :second_root
+
+  def initialize(a, b, c)
+    @a, @b, @c = a, b, c
+    solve
+  end
+
+  def solve
+    outside_of_root = -@b
+    inside_of_root = Math.sqrt((@b)**2 - 4*@a*@c)
+    denominator = 2 * @a
+    find_roots(outside_of_root, inside_of_root, denominator)
+  end
+
+  private
+    def find_roots(outer, inner, denominator)
+      @first_root = plus_route(outer, inner, denominator)
+      @second_root = minus_route(outer, inner, denominator)
+    end
+
+    def plus_route(outer, inner, denom)
+      result = (outer + inner) / denom
+      result.to_r.denominator === 1 ? result.to_i : result.to_r
+    end
+    
+    def minus_route(outer, inner, denom)
+      result = (outer - inner) / denom
+      result.to_r.denominator === 1 ? result.to_i : result.to_r
+    end
+end
