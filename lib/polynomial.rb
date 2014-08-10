a8191d7262d0126061f5338966dcfff1384d3e5e
David Ko <david.ko@damncarousel.com>
2014-08-10 14:56:03 -0400

    Covers the positive coefficient case

--- lib/polynomial.rb
+++ lib/polynomial.rb
@@ -3,5 +3,20 @@ class Polynomial
 
   def initialize(string)
     @elements = string.split(/\+|\-/)
+    map_elements
   end
+
+
+  private
+    def map_elements
+      @elements.map! { |element| { coefficient: coefficient_of(element), exponent: exponent_of(element) } }
+    end
+
+    def coefficient_of(el)
+      el.strip.gsub(/[a-z]\^\d/, '') === '' ? 1 : el.to_i
+    end
+
+    def exponent_of(el)
+      el.strip.gsub(/[a-z]\^/, '').to_i
+    end
 end
--- spec/polynomial_spec.rb
+++ spec/polynomial_spec.rb
@@ -2,19 +2,52 @@ require_relative '../lib/polynomial'
 require 'rspec'
 
 describe Polynomial, 'separate elements' do
-  it 'should parse a quadratic polynomial with all positive elements' do
+  it 'should split a quadratic polynomial with all positive elements' do
     polynomial = Polynomial.new('x^2 + 5x + 3')
     expect(polynomial.elements.count).to eq(3)
   end
 
-  it 'should parse a polynomial with all negative elements' do
+  it 'should split a polynomial with all negative elements' do
     polynomial = Polynomial.new('x^2 - 5x - 3')
     expect(polynomial.elements.count).to eq(3)
   end
 
-  it 'should parse a polynomial with mixed sign elements' do
+  it 'should split a polynomial with mixed sign elements' do
     polynomial = Polynomial.new('x^2 -5x + 3')
     expect(polynomial.elements.count).to eq(3)
   end
 end
 
+describe Polynomial, 'correctly identify components of an element' do
+  before :each do
+    @polynomial = Polynomial.new('x^2 - 5x + 3')
+  end
+
+  it 'should store components in a hash' do
+    expect(@polynomial.elements[0]).to be_an_instance_of Hash
+  end
+
+  describe 'element with exponent' do
+    before :each do
+      @element = @polynomial.elements[0]
+    end
+
+    it 'should properly identify the coefficient' do
+      expect(@element[:coefficient]).to eq(1)
+    end
+
+    it 'should properly identify the exponent' do
+      expect(@element[:exponent]).to eq(2)
+    end
+  end
+
+  describe 'element without exponent' do
+    before :each do
+      @element = @polynomial.elements[1]
+    end
+
+    it 'should properly indentify the coefficient' do
+      expect(@element[:coefficient]).to eq(-5)
+    end
+  end
+end
