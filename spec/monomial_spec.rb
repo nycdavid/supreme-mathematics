f5593e822c7f3cced8e2c939ed9046f3ed287976
David Ko <david.ko@damncarousel.com>
2014-08-15 07:35:27 -0400

    Add specs and code for multiple monomial cases

---
+++ spec/monomial_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../lib/monomial'
+
+describe Monomial, 'basic parsing' do
+  it 'should parse regular mononomial' do
+    input_string = '5x^3'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(5)
+    expect(@monomial.exponent).to eq(3)
+  end
+
+  it 'should parse a negative coefficient' do
+    input_string = '-5x^3'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(-5)
+    expect(@monomial.exponent).to eq(3)
+  end
+
+  it 'should parse a monomial with no exponent' do
+    input_string = '-5x'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(-5)
+    expect(@monomial.exponent).to eq(1)
+  end
+
+  it 'should parse a monomial with no coefficient' do
+    input_string = '-x^2'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(-1)
+    expect(@monomial.exponent).to eq(2)
+  end
+
+  it 'should parse a monomial with no exponent and no coefficient' do
+    input_string = 'x'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(1)
+    expect(@monomial.exponent).to eq(1)
+  end
+
+  it 'should parse a monomial with no exponent and a negative coefficient' do
+    input_string = '-x'
+    @monomial = Monomial.new(input_string)
+    expect(@monomial.coefficient).to eq(-1)
+    expect(@monomial.exponent).to eq(1)
+  end
+end
