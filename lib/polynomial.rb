88a5c8675c4cc138785d9836e5008db6a699d7d4
David Ko <david.ko@damncarousel.com>
2014-08-16 11:23:26 -0400

    Parse indefinite amount of monomial (polynomials)

--- lib/polynomial.rb
+++ lib/polynomial.rb
@@ -6,14 +6,20 @@ class Polynomial
 
   POLYNOMIAL_REGEX = Regexp.new('\A(-?\d*[A-Za-z]?\^?\d*)\s?([\+-]?\s?-?\d*[A-Za-z]?\^?\d*)\z')
 
+  HEADER = '\A'
+  REGEX = '(-?\d*[A-Za-z]?\^?\d*)'
+  DELIMITER = '\s?[\+-]?\s?'
+  FOOTER = '\z'
+
   def initialize(string)
     @input_string = string
+    @regex = Regexp.new(construct_regex(@input_string.scan(/-?\d*[A-Za-z]?\^?\d*\s+[\+-]\s+-?\d*[A-Za-z]?\^?\d*/).count))
     parse_for_elements
   end
 
   private
     def parse_for_elements
-      match = POLYNOMIAL_REGEX.match @input_string
+      match = @regex.match @input_string
       @elements = match.captures.map do |element|
         Monomial.new(remove_whitespace(element))
       end
@@ -21,5 +27,14 @@ class Polynomial
 
     def remove_whitespace(element)
       element = element.gsub(/\s+/, '').gsub('+', '')
+    end
+
+    def construct_regex(count)
+      regex_array = []
+      0.upto(count).each do
+        regex_array << REGEX
+      end
+      regex_array = regex_array.join(DELIMITER)
+      HEADER + regex_array + FOOTER
     end
 end
--- spec/polynomial_spec.rb
+++ spec/polynomial_spec.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 describe Polynomial, 'parsing binomials' do
   before :each do
     @input_string = '-5x + 3'
@@ -14,4 +16,31 @@ describe Polynomial, 'parsing binomials' do
       expect(element).to be_a Monomial
     end
   end
+
+  it 'should properly parse the exponent & coefficient of each element' do
+    @polynomial = Polynomial.new(@input_string)
+    @polynomial.elements.each do |element|
+      expect(element).to be_a Monomial
+    end
+
+    expect(@polynomial.elements[0].coefficient).to eq(-5)
+    expect(@polynomial.elements[0].exponent).to eq(1)
+
+    expect(@polynomial.elements[1].coefficient).to eq(3)
+    expect(@polynomial.elements[1].exponent).to eq(0)
+  end
+
+  it 'should properly parse exponent & coefficient in expression without whitespace' do
+  end
+end
+
+describe Polynomial, 'parsing polynomials of > 2 elements' do
+ #before :each do
+ #  @input_string = '2x^2 - 4x + 6'
+ #end
+
+ #it 'should properly parse the number of elements' do
+ #  @polynomial = Polynomial.new(@input_string)
+ #  expect(@polynomial.elements.count).to eq(3)
+ #end
 end
