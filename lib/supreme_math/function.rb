e35b407aea6371367e1db9c7607f96c81e33450e
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-08-20 14:27:13 -0400

    Pass in var

--- lib/supreme_math/function.rb
+++ lib/supreme_math/function.rb
@@ -4,17 +4,16 @@ module SupremeMath
     # exactly one element, called f(x) in a set E
 
     def evaluate(independent_var)
-      @indep_var = independent_var
-      if @indep_var.is_a? String
+      if independent_var.is_a? String
         algebraically_evaluate
       else
-        numerically_evaluate
+        numerically_evaluate independent_var
       end
     end
 
-    def numerically_evaluate
+    def numerically_evaluate(ivar)
       return elements.inject 0 do |memo, ele|
-        memo += ele.coefficient * (@indep_var**ele.exponent)
+        memo += ele.coefficient * (ivar**ele.exponent)
         memo
       end
     end
