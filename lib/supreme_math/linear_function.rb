037385b93a473041a093c997bb9ac206718430eb
David Ko <david.ko@damncarousel.com>
2014-08-27 07:42:08 -0400

    Make parent class of LinearFunction < Polynomial

--- lib/supreme_math/linear_function.rb
+++ lib/supreme_math/linear_function.rb
@@ -1,9 +1,9 @@
 module SupremeMath
-  class LinearFunction < Function
+  class LinearFunction < Polynomial
 
     attr_reader :slope, :y_intercept
 
-    REGEX = Regexp.new '\A(\d*)[A-Za-z]\s*([\+-]?\s*\d*)\z'
+    REGEX = Regexp.new '\A(-?\d*\.?\d*)[A-Za-z]\s*([\+-]?\s*\d*\.?\d*)\z'
 
     def initialize(input)
       matches = REGEX.match input   
