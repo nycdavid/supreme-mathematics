1a1daada18b14a436fb37e6c2aa83130df5af8d0
David Ko <david.ko@damncarousel.com>
2014-08-28 07:31:03 -0400

    Remove QuadraticFormula lib and tests

--- lib/supreme_math.rb
+++ lib/supreme_math.rb
@@ -3,15 +3,15 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'pry'
 
 require 'bigdecimal'
-require 'supreme_math/quadratic_formula'
 require 'supreme_math/regex_factory'
 require "supreme_math/calculus"
 
 # Classes
 require "supreme_math/function"
-require "supreme_math/polynomial"
 require "supreme_math/monomial"
+require "supreme_math/polynomial"
 require 'supreme_math/linear_function'
+require 'supreme_math/quadratic_function'
 require 'supreme_math/power_function'
 
 module SupremeMath; end
