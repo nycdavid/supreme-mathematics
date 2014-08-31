31dbecfcc3239e89e3f0c722298121754cb4909b
David Ko <david.ko@damncarousel.com>
2014-08-31 10:55:08 -0400

    First spec for Term class

--- lib/supreme_math/term.rb
+++ lib/supreme_math/term.rb
@@ -2,10 +2,13 @@ module SupremeMath
   class Term
     attr_reader :coefficient, :base, :exponent
 
-    def initialize(args)
-      @coefficient = to_numeric(args[0])
-      @base = args[1]
-      @exponent = to_numeric(args[2])
+    REGEX = /\A(-?\d*|-?\d*\.\d+|-?\d*\/\d+?)([A-Za-z]?)\^(\d+|\d+\/\d+)?\z/ 
+
+    def initialize(input)
+      captures = REGEX.match(input).captures
+      @coefficient = to_numeric(captures[0])
+      @base = captures[1]
+      @exponent = to_numeric(captures[2])
     end
 
     private
--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe SupremeMath::Term, 'initialization' do
   it 'should take params in the initializer' do
-    pending
-    captures = SupremeMath::Monomial::REGEX.match('3x^2').captures
-    @term = SupremeMath::Term.new(captures)
+    @term = SupremeMath::Term.new('3x^2')
     
     expect(@term.coefficient).to eq(3)
     expect(@term.base).to eq('x')
