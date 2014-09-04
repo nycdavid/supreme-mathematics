f005841c5991e1c7ffd60cfe38275529694a1cd5
David Ko <david.ko@damncarousel.com>
2014-09-04 07:36:55 -0400

    Change over Monomial to Term class

--- lib/supreme_math/monomial.rb
+++ lib/supreme_math/monomial.rb
@@ -4,61 +4,21 @@ module SupremeMath
     REGEX = /\A(-?\d*\.?\d*)([A-Za-z]?\^?(\d*))\z/
     @@regexes[REGEX] = self
 
-    attr_accessor :coefficient, :exponent, :base
+    attr_accessor :term
 
     def initialize(str)
-      @match = REGEX.match str
-      raise ArgumentError, 'Invalid value for exponent. Must be non-negative/an integer.' unless valid?
-      @base = @match.captures[1]
-      @coefficient, @exponent = parse_for_coefficient_and_exponent(str)
+      @input = str
+      @term = Term.new(str)
+      raise ArgumentError, 'Invalid format for Monomial class.' unless valid?
     end
 
     def to_string
-      coeff_convert(@coefficient) + @base.to_s
+      @input
     end
     
     private
       def valid?
-        @match.respond_to? 'captures' and @match.captures.length === 3
-      end
-
-      def coeff_convert(coefficient)
-        case 
-        when coefficient === -1
-          '-'
-        when coefficient < 0
-          " - #{coefficient.abs.to_s}"
-        when coefficient === 1
-          ''
-        else
-          " + #{coefficient.to_s}"
-        end
-      end
-
-      def parse_for_coefficient_and_exponent(str)
-        return get_coefficient, get_exponent
-      end
-      
-      def get_coefficient
-        case @match.captures[0]
-        when '-'
-          -1
-        when ''
-          1
-        else
-          to_numeric(@match.captures[0])
-        end
-      end
-
-      def get_exponent
-        case @match.captures[1]
-        when ''
-          0
-        when /\A([A-Za-z])\z/
-          1
-        when /\A([A-Za-z]\^(\d))\z/
-          $2.to_i
-        end
+        @term.exponent.value > 0 and @term.exponent.value.is_a? Integer and @term.base.value.is_a? String
       end
 
   end
--- spec/monomial_spec.rb
+++ spec/monomial_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe SupremeMath::Monomial, 'input validations' do
   it 'should not accept negative exponents' do
-    expect { SupremeMath::Monomial.new('x^-2') }.to raise_error('Invalid value for exponent. Must be non-negative/an integer.')
+    expect { SupremeMath::Monomial.new('x^-2') }.to raise_error('Invalid format for Monomial class.')
   end
 end
 
@@ -10,57 +10,43 @@ describe SupremeMath::Monomial, 'basic parsing' do
   it 'should parse regular mononomial' do
     input_string = '5x^3'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(5)
-    expect(@monomial.exponent).to eq(3)
+    expect(@monomial.term.coefficient.value).to eq(5)
+    expect(@monomial.term.exponent.value).to eq(3)
   end
 
   it 'should parse a negative coefficient' do
     input_string = '-5x^3'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(-5)
-    expect(@monomial.exponent).to eq(3)
+    expect(@monomial.term.coefficient.value).to eq(-5)
+    expect(@monomial.term.exponent.value).to eq(3)
   end
 
   it 'should parse a monomial with no exponent' do
     input_string = '-5x'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(-5)
-    expect(@monomial.exponent).to eq(1)
+    expect(@monomial.term.coefficient.value).to eq(-5)
+    expect(@monomial.term.exponent.value).to eq(1)
   end
 
   it 'should parse a monomial with no coefficient' do
     input_string = '-x^2'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(-1)
-    expect(@monomial.exponent).to eq(2)
+    expect(@monomial.term.coefficient.value).to eq(-1)
+    expect(@monomial.term.exponent.value).to eq(2)
   end
 
   it 'should parse a monomial with no exponent and no coefficient' do
     input_string = 'x'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(1)
-    expect(@monomial.exponent).to eq(1)
+    expect(@monomial.term.coefficient.value).to eq(1)
+    expect(@monomial.term.exponent.value).to eq(1)
   end
 
   it 'should parse a monomial with no exponent and a negative coefficient' do
     input_string = '-x'
     @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(-1)
-    expect(@monomial.exponent).to eq(1)
-  end
-
-  it 'should parse a monomial with no exponent and no coefficient' do
-    input_string = '3'
-    @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(3)
-    expect(@monomial.exponent).to eq(0)
-  end
-
-  it 'should parse a monomial with no exponent and no coefficient' do
-    input_string = '-3'
-    @monomial = SupremeMath::Monomial.new(input_string)
-    expect(@monomial.coefficient).to eq(-3)
-    expect(@monomial.exponent).to eq(0)
+    expect(@monomial.term.coefficient.value).to eq(-1)
+    expect(@monomial.term.exponent.value).to eq(1)
   end
 end
 
@@ -68,12 +54,12 @@ describe SupremeMath::Monomial, 'storing the base' do
   it 'should have a base attribute' do
     @monomial = SupremeMath::Monomial.new('x^2')
 
-    expect(@monomial.base).to be_a String
+    expect(@monomial.term.base.value).to be_a String
   end
 
   it 'should store the base of monomial' do
     @monomial = SupremeMath::Monomial.new('x^2')
   
-    expect(@monomial.base).to eq('x^2')
+    expect(@monomial.term.base.value).to eq('x')
   end
 end
