5e048601a4414654d68b2245713b29be31679d47
David Ko <david.ko@damncarousel.com>
2014-09-10 09:00:02 -0400

    Regex initialization from yaml

--- lib/supreme_math/linear_function.rb
+++ lib/supreme_math/linear_function.rb
@@ -2,6 +2,20 @@ module SupremeMath
   class LinearFunction < Polynomial
 
     attr_reader :slope, :y_intercept
+    REGEX_FILE = YAML.load_file('lib/config/regexes/linear_function.yml')
+    REGEX_ARRAY = []
+    REGEX_FILE['slope'].each do |slope_format|
+      REGEX_FILE['operator'].each do |operator_format|
+        REGEX_FILE['intercept'].each do |intercept_format|
+          REGEX_ARRAY << slope_format + operator_format + intercept_format
+          REGEX_ARRAY << intercept_format + operator_format + slope_format
+        end
+      end
+    end
+
+    REGEX_ARRAY.each do |regex|
+      @@regexes[Regexp.new(regex, true)] = self
+    end
 
     def initialize(input)
       super
