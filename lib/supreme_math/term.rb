dfd0890dc06ce59c7e19d1c6e7f752b605b70a2d
David Ko <david.ko@damncarousel.com>
2014-09-14 11:33:56 -0400

    Term class

--- lib/supreme_math/term.rb
+++ lib/supreme_math/term.rb
@@ -1,18 +1,16 @@
 module SupremeMath
   class Term
 
-    REGEX = /([\+-]?\s*\(*-?\d*[\/\.]*\d*\)*[a-z] \^? -?\(?\d*\/?\d*\)?) | ([\+-]? \s* \(*\d[\/\.]*\d*\)*)/ix
+    TERM_REGEX = /([\+-]?\s*\(*-?\d*[\/\.]*\d*\)*[a-z] \^? -?\(?\d*\/?\d*\)?) | ([\+-]? \s* \(*\d*[\/\.]*\d*\)*)/ix
     
     def self.parse(input)
-      captures = REGEX.match(input).captures
+      captures = TERM_REGEX.match(input).captures
       if captures[0].nil?
         SupremeMath::Constant.new(captures[1]) 
       else
         SupremeMath::Variable.new(captures[0])
       end
     end
-
-    private
 
   end
 end
