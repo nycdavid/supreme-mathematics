0bbf5c7d7a81a72d9b0b8c65280dcb6bf67b7373
David Ko <david.ko@damncarousel.com>
2014-09-15 08:22:47 -0400

    All specs passing for Function spec

--- lib/supreme_math/function.rb
+++ lib/supreme_math/function.rb
@@ -3,10 +3,25 @@ module SupremeMath
     # A function f is a rule that assigns to each element x in a set D
     # exactly one element, called f(x) in a set E
 
-    @@regexes = {}
+    TERM_REGEX = /([\+-]?\s*\(*-?\d*[\/\.]*\d*\)*[a-z] \^? -?\(?\d*\/?\d*\)? | [\+-]? \s* \(*\d*[\/\.]*\d*\)*)/ix
 
     def self.parse(input)
-      @@regexes.select { |k, v| k.match input }.values[0].new(input)
+      matches = input.scan(TERM_REGEX).map { |matches| matches.compact }.flatten.delete_if { |match| match.gsub(/\s+/, '') == '' }
+      matches.map! { |match| SupremeMath::Term.parse(match) }
+      term_collection = SupremeMath::TermCollection.new(matches)
+      analyze(input, term_collection)
+    end
+
+    def self.analyze(input, term_collection)
+      if term_collection.all_integer_exponents? && term_collection.highest_exponent == 1
+        SupremeMath::LinearFunction.new(input, term_collection)
+      elsif term_collection.all_integer_exponents? && term_collection.highest_exponent == 2
+        SupremeMath::QuadraticFunction.new(input, term_collection)
+      elsif term_collection.all_integer_exponents? && term_collection.all_positive_exponents?
+        SupremeMath::Polynomial.new(input, term_collection)
+      elsif term_collection.all.count == 1
+        SupremeMath::PowerFunction.new(input, term_collection)
+      end
     end
 
     def to_string
@@ -22,13 +37,8 @@ module SupremeMath
     end
 
     def numerically_evaluate(ivar)
-      return terms.inject 0 do |memo, term|
-        if term.base.value.is_a? String
-          memo += term.coefficient.value * (ivar**term.exponent.value)
-        else
-          memo += term.coefficient.value
-        end
-        memo
+      return terms.variables.inject terms.constant.value do |memo, term|
+        memo += term.coefficient * (ivar**term.exponent)
       end
     end
 
--- spec/function_spec.rb
+++ spec/function_spec.rb
@@ -1,7 +1,14 @@
 require "spec_helper"
 
-describe SupremeMath::Function, 'parse the input string and return the correct object' do
+describe SupremeMath::Function, 'function detection' do
+  it 'should properly detect a linear function (polynomial)' do
+    @linear_function = SupremeMath::Function.parse('3x + 4')
+
+    expect(@linear_function).to be_a SupremeMath::LinearFunction
+  end 
+
   it 'should return a monomial' do
+    pending
     @function = SupremeMath::Function.parse('x^2')
     expect(@function).to be_a SupremeMath::Monomial
   end
@@ -9,19 +16,19 @@ end
 
 describe SupremeMath::Function, 'function evaluation with a numeric value' do
   it 'should evaluate the function f(x) for a given value of x' do
-    @polynomial = SupremeMath::Polynomial.new('x^2 + 3')
+    @polynomial = SupremeMath::Function.parse('x^2 + 3')
     result = @polynomial.evaluate(3)
     expect(result).to eq(12)
   end
 
   it 'should properly evaluate f(x) with a negative coefficient' do
-    @polynomial = SupremeMath::Polynomial.new('-x^2 + 3')
+    @polynomial = SupremeMath::Function.parse('-x^2 + 3')
     result = @polynomial.evaluate(3)
     expect(result).to eq(-6)
   end
 
   it 'should properly evaluate f(x) in a polynomial with elements > 2' do
-    @polynomial = SupremeMath::Polynomial.new('x^2 + 6x - 5')
+    @polynomial = SupremeMath::Function.parse('x^2 + 6x - 5')
     result = @polynomial.evaluate(2)
     expect(result).to eq(11)
   end
@@ -29,17 +36,17 @@ end
 
 describe SupremeMath::Function, 'simplifying a function' do
   it 'should present a string representation of the function' do
-    @polynomial = SupremeMath::Polynomial.new('x^2 + 2')
+    @polynomial = SupremeMath::Function.parse('x^2 + 2')
     expect(@polynomial.to_string).to eq('x^2 + 2')
   end
 
   it 'should combine like terms' do
     pending
-    @polynomial = SupremeMath::Polynomial.new('x^2 + x^2')
+    @polynomial = SupremeMath::Function.parse('x^2 + x^2')
 
-    expect(@polynomial.terms.count).to eq(1)
-    expect(@polynomial.terms.first.coefficient.value).to eq(2)
-    expect(@polynomial.terms.first.base.value).to eq('x')
+    expect(@polynomial.terms.all.count).to eq(1)
+    expect(@polynomial.terms.all.first.coefficient.value).to eq(2)
+    expect(@polynomial.terms.all.first.base.value).to eq('x')
     expect(@polynomial.terms.first.exponent.value).to eq(2)
   end
 end
@@ -51,13 +58,5 @@ describe SupremeMath::Function, 'abstract/algebraic function evaluation' do
     result = @polynomial.evaluate('x + 2')
     expect(result).to be_a SupremeMath::Polynomial
     expect(result).to eq('(x + 2)^2 + 3')
-  end
-end
-
-describe SupremeMath::Function, 'function detection' do
-  it 'should properly detect a linear function (polynomial)' do
-    @linear_function = SupremeMath::Function.parse('3x + 4')
-
-    expect(@linear_function).to be_a SupremeMath::LinearFunction
   end
 end
