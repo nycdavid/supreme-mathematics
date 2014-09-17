c691eab77b489ed2287bd9d18d4ef5b5735f2082
David Ko <david.ko@damncarousel.com>
2014-09-17 08:04:23 -0400

    Accept and coerce string representations of integers and rationals

--- lib/supreme_math/independent_variable.rb
+++ lib/supreme_math/independent_variable.rb
@@ -1,5 +1,21 @@
 module SupremeMath
   class IndependentVariable
+    attr_reader :value
+
+    FORMATS = {
+      /\A\d+\z/ => 'to_i',
+      /\A\(\d+\/\d+\)\z/ => 'to_r'
+    }
+    
+    def initialize(input)
+      @input = input
+      @value = delint(@input).__send__(FORMATS.select { |k, v| k.match @input }.values[0])
+    end
+
+    private
+      def delint(input)
+        input.gsub(/\(|\)/, '')
+      end
 
   end
 end
--- spec/independent_variable_spec.rb
+++ spec/independent_variable_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe SupremeMath::IndependentVariable, 'initialization' do
+  it 'should accept and coerce integer IVars' do
+    ivar = SupremeMath::IndependentVariable.new('3')
 
+    expect(ivar.value).to be_a Integer
+    expect(ivar.value).to eq(3)
+  end
+
+  it 'should accept and coerce rationals' do
+    ivar = SupremeMath::IndependentVariable.new('(1/3)')
+
+    expect(ivar.value).to be_a Rational
+    expect(ivar.value).to eq(1/3.to_r)
+  end
 end
