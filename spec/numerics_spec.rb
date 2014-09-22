ea6cb844e53b7812cb45095ae53691af8047551a
David Ko <david.ko@damncarousel.com>
2014-09-22 07:41:05 -0400

    Rational subtraction test case

--- spec/numerics_spec.rb
+++ spec/numerics_spec.rb
@@ -129,5 +129,12 @@ describe Numerics, 'expressions' do
 
       expect(result).to eq(5/8.to_r)
     end
+
+    it 'should evaluate rational subtraction' do
+      expression = '7/8 - 1/8 - 3/8'
+      result = @parser.parse(expression).evaluate
+
+      expect(result).to eq(3/8.to_r)
+    end
   end
 end
