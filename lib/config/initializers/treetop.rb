dbd0e7eaa740c91a357429f16f8f698410aaffc2
David Ko <david.ko@damncarousel.com>
2014-10-08 07:52:39 -0400

    Load files individually in order to ensure dependencies are fulfilled

--- lib/config/initializers/treetop.rb
+++ lib/config/initializers/treetop.rb
@@ -1,5 +1,5 @@
 require 'treetop'
 
-Dir.glob('./lib/config/grammars/*.treetop').each do |grammar_file|
-  Treetop.load grammar_file
-end
+Treetop.load './lib/config/grammars/numerics.treetop'
+Treetop.load './lib/config/grammars/polynomial.treetop'
+Treetop.load './lib/config/grammars/function.treetop'
