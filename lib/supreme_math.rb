3d80156501ef271c1b3844d4677be9c10cf30440
David Ko <david.ko@damncarousel.com>
2014-10-10 07:27:45 -0400

    Comment out all classes for now

--- lib/supreme_math.rb
+++ lib/supreme_math.rb
@@ -8,22 +8,22 @@ require 'config/initializers/treetop'
 require "supreme_math/calculus"
 
 # Classes
-require 'supreme_math/term'
-require 'supreme_math/constant'
-require 'supreme_math/variable'
-require 'supreme_math/coefficient'
-require 'supreme_math/base'
-require 'supreme_math/exponent'
-require 'supreme_math/independent_variable'
-require 'supreme_math/supreme_rational'
-require 'supreme_math/rational_function'
-require 'supreme_math/term_collection'
-require "supreme_math/function"
-require "supreme_math/monomial"
-require "supreme_math/polynomial"
-require 'supreme_math/linear_function'
-require 'supreme_math/quadratic_function'
-require 'supreme_math/power_function'
+# require 'supreme_math/term'
+# require 'supreme_math/constant'
+# require 'supreme_math/variable'
+# require 'supreme_math/coefficient'
+# require 'supreme_math/base'
+# require 'supreme_math/exponent'
+# require 'supreme_math/independent_variable'
+# require 'supreme_math/supreme_rational'
+# require 'supreme_math/rational_function'
+# require 'supreme_math/term_collection'
+# require "supreme_math/function"
+# require "supreme_math/monomial"
+# require "supreme_math/polynomial"
+# require 'supreme_math/linear_function'
+# require 'supreme_math/quadratic_function'
+# require 'supreme_math/power_function'
 
 module SupremeMath; end
 
