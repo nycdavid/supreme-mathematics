c2691eccf588e4284f15ec390f6d9186151a62af
David Ko <david.ko@damncarousel.com>
2014-10-15 08:13:35 -0400

    Move rational out of node extensions

--- lib/supreme_math/numerics.rb
+++ lib/supreme_math/numerics.rb
@@ -10,10 +10,16 @@ module Numerics
       clean_text_value.to_f
     end
   end
+
+  class RationalLiteral < Treetop::Runtime::SyntaxNode
+    def value
+      clean_text_value.to_r
+    end
+  end
 end
 
 class Treetop::Runtime::SyntaxNode
   def clean_text_value
-    text_value.gsub(/\s+/, '')
+    text_value.gsub(/\(/, '').gsub(/\)/, '').gsub(/\s+/, '')
   end
 end
